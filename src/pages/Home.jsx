d092c9cad286a655e20a14677c8e6f5558eb47db
Thanachit Sengsalee <47846297+6010110455@users.noreply.github.com>
2023-02-18 12:22:05 +0700

    add teg meta in HOME page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,22 @@ const Home = () => {
         <title>THANACHIT</title>
         <meta
           name="description"
-          content="This is a website that gathers Thanachit's works and talents."
+          content="This is a website that gathers Thanachit's projects and skills."
+        />
+        {/* Facebook meta tags below */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="benz.sengsalee.com" />
+        <meta
+          property="og:title"
+          content="My portfolio - Hi, i am Thanachit Sengsalee."
+        />
+        <meta
+          property="og:description"
+          content="This is a website that gathers Thanachit's projects and skills."
+        />
+        <meta
+          property="og:image"
+          content="https://storage.googleapis.com/mydev_eonlineshop/others/thumbnails/1676697057106_1024x1024.png"
         />
         <meta
           name="google-site-verification"
